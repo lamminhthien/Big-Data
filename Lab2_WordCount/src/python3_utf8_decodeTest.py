f684b3098b4551049c5ccd622aa9d70cc36cc4c9
lamminhthien <lamminhthien02012000@gmail.com>
2022-03-24 19:13:45 +0700

    Encode and decode utf-8

--- Lab2_WordCount/src/python3_utf8_decodeTest.py
+++ Lab2_WordCount/src/python3_utf8_decodeTest.py
@@ -1,2 +1,7 @@
-a = b'h\xc6\xb0\xc6\xa1ng'
-print(a.decode("utf-8","replace"))
+s = 'h\xc6\xb0\xc6\xa1ng'
+# Encode to windows-1258 | Vietnamese to keep orginal byte like string
+s = s.encode('cp1258')
+# Decode to utf-8 with  errors = replace 
+print(s.decode("utf-8","replace"))
+
+
