ea57f68285a2c025280f0181b92f745a85cad6c6
lamminhthien <lamminhthien02012000@gmail.com>
2022-03-29 09:16:38 +0700

    .

--- Popular bash command.sh
+++ Popular bash command.sh
@@ -1,5 +1,6 @@
 hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.2.2.jar  -file mapper.py -mapper mapper.py  -file reducer.py -reducer reducer.py  -input /user/hdoop/data/elonmusk  -output /user/hdoop/data/elonmusk_tweet_output-top20-word!  -cmdenv LC_CTYPE=vi_VN.UTF-8
 
 
+hdfs dfs -mkdir -p data/lab3
 hdfs dfs -chmod -R 777 /user/hdoop/data
 
