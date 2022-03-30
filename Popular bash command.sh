3b325c088f7f85cc3f2f19a9407ca432457a5657
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-03-30 14:16:27 +0700

    data ncdc

--- Popular bash command.sh
+++ Popular bash command.sh
@@ -1,6 +1,10 @@
 hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.2.2.jar  -file mapper.py -mapper mapper.py  -file reducer.py -reducer reducer.py  -input /user/hdoop/data/elonmusk  -output /user/hdoop/data/elonmusk_tweet_output-top20-word!  -cmdenv LC_CTYPE=vi_VN.UTF-8
 
+hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.2.2.jar  -file mapper.py -mapper mapper.py  -file reducer.py -reducer reducer.py  -input /user/hdoop/data/elonmusk/elon_tweet.csv  -output /user/hdoop/data/elonmusk_tweet_time_elon_tweet_most -cmdenv LC_CTYPE=vi_VN.UTF-8
+
+
 
 hdfs dfs -mkdir -p data/lab3
+
 hdfs dfs -chmod -R 777 /user/hdoop/data
 
