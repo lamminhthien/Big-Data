7d85b0c54c1c89b558a55c8b9792ffde26ff6e62
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-08 10:19:51 +0700

    Xong bài đếm từ in ra 20 từ nhiều nhất = pyspark

--- Lab4_Spark/src/BaiTap2.py
+++ Lab4_Spark/src/BaiTap2.py
@@ -13,10 +13,14 @@ words = lines.flatMap(lambda line: line.split(" "))
 
 # count the occurrence of each word
 wordFrequencies = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+# Sorted decreasing by value, take only 20 result first
+wordFrequencies = wordFrequencies.sortBy(lambda x: x[1],ascending=False).take(20)
 
-def g(x):
-    print(x)
-wordFrequencies.foreach(g)
+# def g(x):
+#     print(x)
+# wordFrequencies.foreach(g)
+print(type(wordFrequencies))
+print(wordFrequencies)
 
 # # save the set of <word, frequency> to disk
 # savingPath = "/home/hdoop/pyspark-output"
