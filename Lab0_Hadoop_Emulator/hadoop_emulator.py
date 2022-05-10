ed0e57026fe640e86ec07f272062e6a457b8ede6
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-10 20:58:35 +0700

    Sữa lỗi bị dư dòng rỗng khi chạy hadoop emulator

--- Lab0_Hadoop_Emulator/hadoop_emulator.py
+++ Lab0_Hadoop_Emulator/hadoop_emulator.py
@@ -2,8 +2,10 @@
 import sys
 # Tạo array mô phỏng lại quá trình shuffing bên hadoop
 shuffing_emulator = []
-# Đọc output được tạo ra từ file mapper.py
+# Đọc output được tạo ra từ file mapper.py và đưa vào mảng để chuẩn bị shuffing
 for line in sys.stdin:
     shuffing_emulator.append(line)
     # print(line)
-print(sorted(shuffing_emulator))
+# Bắt đầu shuffing
+for element in sorted(shuffing_emulator):
+    print(element, sep=' ', end='', flush=True)
