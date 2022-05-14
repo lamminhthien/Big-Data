ad7e857cbf31c29ba19f98e1b9c628b3cb38f990
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-14 12:06:26 +0700

    Bai 9

---
+++ ThucHanhOnThi/Bai9/bai9.py
@@ -0,0 +1,28 @@
+from gc import collect
+import os, shutil
+from pyspark import SparkContext
+import re
+import sys
+sc = SparkContext("local", "Text processing with PySpark Example")
+
+(date_when_max_temp,max_temp) = (None,-sys.maxsize)
+(current_date, last_year) = (None,None)
+lines = sc.textFile("/mnt/e/HỌC/BT-Bigdata/Big-Data-main/Lab3_NCDC_WeatherData/data/preprocessed/1902.txt").cache()
+for line in lines.collect():
+    year = line[15:19]
+    date = line[15:23]
+    temp = line[87:92]
+    
+    if last_year != None and last_year != year:
+         print("%s\t%s\t%s" % (last_year, date,max_temp))
+         (last_year, max_temp) = (year, int(temp))
+    else:
+        last_year = year
+        temp_integer = int(temp)
+        if (temp_integer > max_temp):
+            max_temp = temp_integer
+            date_when_max_temp = date
+
+if last_year:
+  print("%s\t%s\t%s" % (last_year, date,max_temp))
+    
