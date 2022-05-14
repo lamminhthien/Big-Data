5ee0bb75563fe8e14aff7e64da06dd0c8a679a04
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-14 11:17:36 +0700

    Bai 5

---
+++ ThucHanhOnThi/Bai5/reducer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import sys
+
+sum = 0
+count = 0
+
+last_key = None
+for line in sys.stdin:
+  (key, val) = line.strip().split("\t")
+  sum = sum + int(val)
+  count = count + 1
+  if last_key and last_key != key:
+    print(f"{last_key}\t{round(sum/count)}")
+    last_key = key
+    sum = 0
+    count = 0
+  else:
+    last_key = key
+
+if last_key:
+  print(f"{last_key}\t{}\t{}\t{round(sum/count)}")
